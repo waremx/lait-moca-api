1ccd56c0e94c21ac5aa0c1a110f5958c94e3ddef
ymoyamac <yael.moya.mac@outlook.com>
2026-06-21 19:59:58 -0600

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(say_hello));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn say_hello() -> String {
+    "Hello, World!".to_string()
 }
